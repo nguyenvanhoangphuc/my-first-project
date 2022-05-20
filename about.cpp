38a6c2952374c76bbdde772508fab0d42d1a1fd6
TTTPhuong122 <tttp2002@gmail.com>
2022-05-20 15:23:48 +0700

    Phuong commit

--- about.cpp
+++ about.cpp
@@ -1,9 +1,1 @@
-#include <stdio.h>
-
-int main(int argc, char const *argv[])
-{
-    printf("Phuong");
-    printf("phep toan so hoc"); 
-    printf("hinh hoc khong gian vecto va hinh phang"); 
-    return 0;
-}
+//phuong
