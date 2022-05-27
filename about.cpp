d93553a9e57a059641897f0532d77a49a3a58857
toilaphucnguyen <83715016+nguyenvanhoangphuc@users.noreply.github.com>
2022-05-27 17:02:04 +0700

    phuc commit lan n

--- about.cpp
+++ about.cpp
@@ -6,5 +6,6 @@ int main(int argc, char const *argv[])
     printf("phep toan so hoc"); 
     printf("toi la phuc ne"); 
     printf("hinh hoc khong gian vecto va hinh phang"); 
+    printf("Phuc viet dong nay"); 
     return 0;
 }
